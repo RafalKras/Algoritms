e5e2a51785521190601ecbc5bfe2707d66cc2b60
Rafal Kras <rafal.kras@gmail.com>
2019-09-22 22:22:35 +0200

    Insertion Sort added for int

--- src/sorting/App.java
+++ src/sorting/App.java
@@ -6,7 +6,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] arr1 = new int[]{2,9,45,2000,-2,85,74,641,-999999999};
+		int[] arr1 = new int[]{2,9,45,20,-2,85,74,641,-9,0,0,0,0,1,5,6,8,7,4,20};
 		arr1 = InsertionSort.iSort(arr1);
 		
 		for(int c: arr1) {
