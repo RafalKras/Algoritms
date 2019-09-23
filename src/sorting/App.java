18f7e30d3e674383056d4113fda5bf51fbfc6793
Rafal Kras <rafal.kras@gmail.com>
2019-09-23 05:19:27 +0200

    message

--- src/sorting/App.java
+++ src/sorting/App.java
@@ -4,14 +4,25 @@ package sorting;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
+		// use insertion sort
 		int[] arr1 = new int[]{2,9,45,20,-2,85,74,641,-9,0,0,0,0,1,5,6,8,7,4,20};
 		arr1 = InsertionSort.iSort(arr1);
 		
+		System.out.println("Uses Insertion Sort to sort Array");
 		for(int c: arr1) {
 			System.out.print( c +" ");
 		}
+		System.out.println();
+		// use Bubble sort
+		System.out.println("Uses bubbleSort to sort array.");
+		int[] arr2 = new int[]{5,2,3,-9,-25,-6,0,0,4,8,12,20,26};
+		arr2 = BubbleSortArray.bsort(arr2);
+		for(int c: arr2) {
+			System.out.print( c +" ");
+		}
+		System.out.println();
 	}
 
 }
